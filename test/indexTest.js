7b904a5dfcebebe02d11905dd2732e51669f1bea
pwangy <blipsandclicks@gmail.com>
2023-12-20 18:02:30 -0800

    remove semicolins

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,25 +1,25 @@
-require ( './root.js' );
+require ( './root.js' )
 
-const { name, height, message } = require("../index.js");
+const { name, height, message } = require("../index.js")
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
     it('returns "Susan"', () => {
       expect(name).toEqual("Susan");
-    });
-  });
+    })
+  })
 
   describe("Height", () => {
     it("is less than 40 and greater than 0", () => {
       expect(height).toBeMoreThan(0)
       expect(height).toBeLessThan(40);
-    });
-  });
+    })
+  })
 
   describe("Message", () => {
     it("gives the name and height", () => {
-      expect(message).toInclude(name);
-      expect(message).toInclude(height);
-    });
-  });
-});
+      expect(message).toInclude(name)
+      expect(message).toInclude(height)
+    })
+  })
+})
